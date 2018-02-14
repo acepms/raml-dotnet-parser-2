bf335b5e5939fc1a4b454db2b313576e52fa8676
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-02-14 17:54:21 -0300

    initial model

---
+++ source/ClassLibrary1/Model/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary1.Model
+{
+    public class Shape
+    {
+        public Shape(string name, string displayName, string description, string @default, IEnumerable<string> values, IEnumerable<Shape> inherits)
+        {
+            Name = name;
+            DisplayName = displayName;
+            Description = description;
+            Default = @default;
+            Values = values;
+            Inherits = inherits;
+        }
+
+        public string Name { get; }
+        public string DisplayName { get; }
+        public string Description { get; }
+        public string Default { get; }
+        public IEnumerable<string> Values { get; }
+        public IEnumerable<Shape> Inherits { get; }
+    }
+}
