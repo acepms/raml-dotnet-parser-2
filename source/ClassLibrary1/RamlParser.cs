8ae206d3bac0b82d0638dccacdc833ee1c64c481
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-02-16 16:19:45 -0300

    map init

--- source/ClassLibrary1/RamlParser.cs
+++ source/ClassLibrary1/RamlParser.cs
@@ -1,18 +1,25 @@
-﻿using EdgeJs;
+﻿using ClassLibrary1.Mappers;
+using ClassLibrary1.Model;
+using EdgeJs;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Dynamic;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
     public class RamlParser
     {
-        public object Load(string filePath)
+        public WebApi Load(string filePath)
         {
             var rawresult = GetDynamicStructureAsync(filePath).Result;
-            return rawresult;
+            var ret = rawresult as IDictionary<string, object>;
+            var error = ret["error"];
+            if (error != null)
+                throw new FormatException(error as string);
+            var model = ret["model"] as IDictionary<string, object>;
+            var webApi = new WebApiMapper().Map(model);
+            return webApi;
         }
 
         public static async Task<object> GetDynamicStructureAsync(string filePath)
