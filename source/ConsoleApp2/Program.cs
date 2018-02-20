19029ae0fcf18e63e9509834c2b5162501ea8b8f
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-02-20 14:49:18 -0300

    tests + fixes

--- source/ConsoleApp2/Program.cs
+++ source/ConsoleApp2/Program.cs
@@ -1,9 +1,11 @@
 ﻿using ClassLibrary1;
+using ClassLibrary1.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestProject1;
 
 namespace ConsoleApp2
 {
@@ -11,14 +13,38 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            var a = Test().Result;
-            Console.WriteLine(a);
+            //var a = Test().Result;
+            //Console.WriteLine(a);
+            RunTests();
         }
 
-        private async static Task<ClassLibrary1.Model.WebApi> Test()
+        private static void RunTests()
+        {
+            var tests = new MoviesTests();
+            try
+            {
+                tests.Initialize();
+                tests.Movies_Endpoints_should_be_9();
+                tests.Movies_Post_Operation_Security();
+                tests.Movies_schemes_should_be_http();
+                tests.Movies_basepath_should_be_api();
+                tests.Movies_version_should_be_1();
+                tests.Movies_get_response();
+                Console.WriteLine("Succeeded");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Failed");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        private async static Task<WebApi> Test()
         {
             var parser = new RamlParser();
             var a = await parser.Load("file:///desarrollo/mulesoft/raml-dotnet-parser-2/source/Raml.Parser.Tests/Specifications/movies-v1.raml");
+
             return a;
         }
     }
---
+++ source/UnitTestProject1/MoviesTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System.Linq;
+using ClassLibrary1.Model;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MoviesTests
+    {
+        private WebApi model;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var parser = new RamlParser();
+            model = parser.Load("file://./specs/movies-v1.raml").Result;
+        }
+
+        [TestMethod]
+        public void Movies_Endpoints_should_be_9()
+        {
+            Assert.AreEqual(9, model.EndPoints.Count());
+        }
+
+        [TestMethod]
+        public void Movies_schemes_should_be_http()
+        {
+            Assert.AreEqual(1, model.Schemes.Count());
+            Assert.AreEqual("HTTP", model.Schemes.First());
+        }
+
+        [TestMethod]
+        public void Movies_version_should_be_1()
+        {
+            Assert.AreEqual("1.0", model.Version);
+        }
+
+        [TestMethod]
+        public void Movies_basepath_should_be_api()
+        {
+            Assert.AreEqual("/api", model.BasePath);
+        }
+
+        [TestMethod]
+        public void Movies_host_should_be_movies_com()
+        {
+            Assert.AreEqual("movies.com", model.Host);
+        }
+
+        [TestMethod]
+        public void Movies_name_should_be_movies_v_1()
+        {
+            Assert.AreEqual("Movies v 1", model.Name);
+        }
+
+        [TestMethod]
+        public void Movies_Post_Operation_Security()
+        {
+            Assert.AreEqual(1, model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.Count());
+            Assert.AreEqual("OAuth 2.0", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Type);
+            Assert.AreEqual("https://localhost:8081/oauth/authorize", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Settings.AuthorizationUri);
+            Assert.AreEqual("https://localhost:8081/oauth/access_token", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Settings.AccessTokenUri);
+            Assert.AreEqual("authorization_code", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Settings.AuthorizationGrants.First());
+            Assert.AreEqual("read", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Settings.Scopes.First().Name);
+            Assert.AreEqual("write", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Settings.Scopes.Last().Name);
+            Assert.AreEqual("access_token", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().QueryParameters.First().Name);
+            Assert.AreEqual("Authorization", model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "post").Security.First().Headers.First().Name);
+        }
+
+        [TestMethod]
+        public void Movies_get_response()
+        {
+            var resp = model.EndPoints.First(e => e.Path == "/movies").Operations.First(o => o.Method == "get").Responses.First();
+            Assert.AreEqual("200", resp.StatusCode);
+            Assert.AreEqual(1, resp.Payloads.Count());
+            Assert.AreEqual("application/json", resp.Payloads.First().MediaType);
+
+        }
+    }
+}
